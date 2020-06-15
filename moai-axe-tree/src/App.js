c6b20a2548274214c69888aa93f2aac633902d53
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-06-15 00:29:23 -0700

    initial state

--- moai-axe-tree/src/App.js
+++ moai-axe-tree/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
+import Game from './components/Game';
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
+      <Game />
     </div>
   );
 }
